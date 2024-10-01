ef0e4fb5dab46607101735c92f1d096880dd82d2
Marcos <202202382@alu.comillas.com>
2024-10-01 10:46:16 +0200

    Creating a class for vehicles with plates

---
+++ PlateVehicle.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice1
+{
+    internal abstract class PlateVehicle : Vehicle
+    {
+        private string typeOfVehicle;
+        private string plate;
+        public PlateVehicle(string typeOfVehicle, string plate) : base(typeOfVehicle)
+        {
+            this.typeOfVehicle = typeOfVehicle;
+            this.plate = plate;
+        }
+
+        public string GetPlate()
+        {
+            return plate;
+        }
+        public override string ToString()
+        {
+            return $"{GetTypeOfVehicle()} with plate {GetPlate()}";
+        }
+    }
+
+}
