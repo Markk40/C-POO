dcb074cb67e9ab75f9d2f0cc37b093d97515122f
Marcos <202202382@alu.comillas.com>
2024-10-01 16:14:15 +0200

    Creating the class fot City

---
+++ City.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice1
+{
+    internal class City
+    {
+        private List<Taxi> taxiLicenses;
+        private PoliceStation policeStation;
+
+        public City()
+        {
+            taxiLicenses = new List<Taxi>();
+            policeStation = new PoliceStation();
+        }
+
+        public List<Taxi> GetTaxiLicenses()
+        {
+            return taxiLicenses;
+        }
+
+        public void AddTaxi(Taxi taxi)
+        {
+            taxiLicenses.Add(taxi);
+        }
+        public void RemoveTaxi(Taxi taxi)
+        {
+            taxiLicenses.Remove(taxi);
+        }
+    }
+}
