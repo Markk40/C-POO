5126539edcdbc5d49e07e2427f7a77bf19b31add
Marcos <202202382@alu.comillas.com>
2024-10-01 18:21:39 +0200

    Creating the Scooter class

---
+++ Scooter.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice1
+{
+    internal class Scooter: NoPlateVehicle
+    {
+        private const string typeOfVehicle = "Scooter";
+        public Scooter():base(typeOfVehicle) { }
+    }
+}
