21fb21a9ba89891fe86da7c4eeed62f720426e44
Marcos <202202382@alu.comillas.com>
2024-10-04 11:29:45 +0200

    Adding sequence of requiered actions to show the functionality of our project

--- Main.cs
+++ Main.cs
@@ -5,36 +5,54 @@
 
         static void Main()
         {
+            PoliceStation station1 = new PoliceStation("Station 1");
+            City city1 = new City("Madrid", station1);
+           
             Taxi taxi1 = new Taxi("0001 AAA");
             Taxi taxi2 = new Taxi("0002 BBB");
-            PoliceCar policeCar1 = new PoliceCar("0001 CNP");
-            PoliceCar policeCar2 = new PoliceCar("0002 CNP");
+            PoliceCar policeCar1 = new PoliceCar("0001 CNP",true,station1);
+            PoliceCar policeCar2 = new PoliceCar("0002 CNP", true, station1);
+            PoliceCar policeCar3 = new PoliceCar("0003 CNP", false, station1);
 
+
+            Console.WriteLine(city1.WriteMessage("Created"));
+            Console.WriteLine(station1.WriteMessage($"Created and added to city {city1}"));
             Console.WriteLine(taxi1.WriteMessage("Created"));
             Console.WriteLine(taxi2.WriteMessage("Created"));
-            Console.WriteLine(policeCar1.WriteMessage("Created"));
-            Console.WriteLine(policeCar2.WriteMessage("Created"));
+            Console.WriteLine(policeCar1.WriteMessage("Created with radar"));
+            Console.WriteLine(policeCar2.WriteMessage("Created with radar"));
+            Console.WriteLine(policeCar3.WriteMessage("Created"));
+
+            city1.AddTaxi(taxi1);
+            city1.AddTaxi(taxi2);
+
+            Console.WriteLine(city1.WriteMessage($"{taxi1} license added"));
+            Console.WriteLine(city1.WriteMessage($"{taxi2} license added"));
+
+            station1.RegisterCar(policeCar1);
+            station1.RegisterCar(policeCar2);
+            station1.RegisterCar(policeCar3);
+
+            Console.WriteLine(station1.WriteMessage($"{policeCar1} registered"));
+            Console.WriteLine(station1.WriteMessage($"{policeCar2} registered"));
+            Console.WriteLine(station1.WriteMessage($"{policeCar3} registered"));
+
 
             policeCar1.StartPatrolling();
-            policeCar1.UseRadar(taxi1);
-
-            taxi2.StartRide();
-            policeCar2.UseRadar(taxi2);
             policeCar2.StartPatrolling();
-            policeCar2.UseRadar(taxi2);
-            taxi2.StopRide();
-            policeCar2.EndPatrolling();
+            policeCar3.StartPatrolling();
 
-            taxi1.StartRide();
-            taxi1.StartRide();
-            policeCar1.StartPatrolling();
+            policeCar3.UseRadar(taxi1);
+
             policeCar1.UseRadar(taxi1);
-            taxi1.StopRide();
-            taxi1.StopRide();
-            policeCar1.EndPatrolling();
 
-            policeCar1.PrintRadarHistory();
-            policeCar2.PrintRadarHistory();
+            taxi1.StartRide();
+            policeCar1.UseRadar(taxi1);
+
+            taxi1.StopRide();
+            city1.RemoveTaxi(taxi1);
+            Console.WriteLine(city1.WriteMessage($"{taxi1} license removed"));
+
 
         }
     }
